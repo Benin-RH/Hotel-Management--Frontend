55458904def361e8643ce9e682fd6c3e762cfac3
Benin-RH <128675525+Benin-RH@users.noreply.github.com>
2024-08-27 11:50:21 +0530

    ggg

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Userregistration from './Component/Userregistration';
 import Useravailability from './Component/Useravailability';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
-
+import Forgot2 from './Component/Forgot2';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Userlogin from './Component/Userlogin';
 function App() {
   return (
-    <div>
-          <Userregistration />
-      
-      
-      <Useravailability/>
-      
+    <BrowserRouter>
+    <div >
+      <Routes>
+      <Route path="userreg" element={<Userregistration/>}/>
+      <Route path="useravl" element={<Useravailability/>}/>
+      <Route path="userlogin" element={<Userlogin/>}/>
+      </Routes>
     </div> 
+    </BrowserRouter>
   );
 }
 
---
+++ src/Component/Userlogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Userlogin.css'
+function Userlogin() {
+  return (
+    <div className='uloginborder'>
+        {/* <link to={"userlogin"}></link> */}
+    </div>
+  )
+}
+
+export default Userlogin
